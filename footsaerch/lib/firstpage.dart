ed6f994535f92a5ad729c090be29a8ea237b4801
disco6057 <disco6057@gmail.com>
2020-07-13 09:12:23 +0900

    testSave

--- footsaerch/lib/firstpage.dart
+++ footsaerch/lib/firstpage.dart
@@ -1,9 +1,5 @@
-import 'dart:math';
-
 import "package:animator/animator.dart";
-
 import 'package:flutter/material.dart';
-
 import 'package:html/parser.dart' show parse;
 import 'package:http/http.dart' as http;
 import 'MyWebView.dart';
